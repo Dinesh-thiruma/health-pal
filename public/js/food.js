ec9d580748867bfd841895be4f61fc9bd2e62882
Jonathan Turnage <jetx2turnage@gmail.com>
2021-08-02 19:03:30 +0000

    Added tile click functionality

--- public/js/food.js
+++ public/js/food.js
@@ -1,3 +1,16 @@
 const loadFoodSchedule = () => {
-    
+
 }
+
+function editDay(tile) {
+    editDayModal = document.getElementById("editDayModal");
+    editDayModal.classList.toggle("is-active");
+}
+
+document.querySelectorAll(".tile").forEach((tile) =>{
+    tile.addEventListener("click", click => {
+        console.log("Hype");
+        editDay(tile);
+    })
+    console.log(tile);
+});
