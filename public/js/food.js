72814340710c811595984d0712b21a8787fa60fa
Jonathan Turnage <jetx2turnage@gmail.com>
2021-08-02 19:49:33 +0000

    Added modals for each day

--- public/js/food.js
+++ public/js/food.js
@@ -3,13 +3,19 @@ const loadFoodSchedule = () => {
 }
 
 function editDay(tile) {
+    editModalTitle = document.getElementById("modalTitle");
+    editModalTitle.value = "That day";
     editDayModal = document.getElementById("editDayModal");
     editDayModal.classList.toggle("is-active");
 }
 
-document.querySelectorAll(".tile").forEach((tile) =>{
+function closeDayModal() {
+    closeDayModal = document.getElementById("editDayModal");
+    closeDayModal.classList.toggle("is-active");
+}
+
+document.querySelectorAll(".tile:not(.is-ancestor)").forEach((tile) =>{
     tile.addEventListener("click", click => {
-        console.log("Hype");
         editDay(tile);
     })
     console.log(tile);
